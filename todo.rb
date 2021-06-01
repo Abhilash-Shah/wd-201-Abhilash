3da0f353c4250236633398c4048ddf9ff47bf711
Abhilash-Shah <abhilashshah1999@gmail.com>
2021-06-01 19:12:22 +0530

    Add my first todo.rb to git

---
+++ todo.rb
@@ -0,0 +1,9 @@
+puts "Jacky Baby"
+puts "hello world!!"
+puts "aaaaaaaaaaaaaaaaaaaaa"
+puts "qqqqqqqqqqqqqqqqqqqqqqqqq  #{1+10}"
+puts "hello"
+puts "What is your next todo?"
+next_todo = gets
+puts "\n\n\n\n"#carriage Return
+puts "[ ]" "#{next_todo} #{next_todo}" #String Interpolation
